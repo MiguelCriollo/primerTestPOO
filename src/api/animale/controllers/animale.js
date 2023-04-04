fc58f87dfa52617e5f63660f3a1e023cc3c0b4c8
Miguell Criollo <macriollo.2@sudamericano.edu.ec>
2023-04-03 22:51:46 -0500

    Primer Datos Strapi POO

---
+++ src/api/animale/controllers/animale.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * animale controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::animale.animale');
---
+++ src/api/animale/routes/animale.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * animale router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::animale.animale');
